9df9cb6430d50899a2d03c877b364d013711f521
Den-Aiven <ivanari432@gmail.com>
2024-09-15 23:01:27 +0700

    Update tugas 1 mengurutkan bilangan.c

--- tugas 1 mengurutkan bilangan.c
+++ tugas 1 mengurutkan bilangan.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
 
 int main() {
-
     int jumlah;
-   
-    int temp = 0;
+    
+    
     scanf("%d", &jumlah);
-    int nilai[jumlah];
-
-    for(int i = 0; i < jumlah; i++){
+    
+    
+    int *nilai = (int *)malloc(jumlah * sizeof(int));
+    
+    if (nilai == NULL) {
+        
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    
+   
+    for (int i = 0; i < jumlah; i++) {
         scanf("%d", &nilai[i]);
     }
     
-    for(int i = 0; i < jumlah-1; i++){
-        for(int j = 0; j<jumlah-i-1; j++){
-            if(nilai[j+1] < nilai[j] ){
-                temp = nilai[j+1];
-                nilai[j+1] = nilai[j];
+    
+    for (int i = 0; i < jumlah - 1; i++) {
+        for (int j = 0; j < jumlah - i - 1; j++) {
+            if (nilai[j + 1] < nilai[j]) {
+                
+                int temp = nilai[j + 1];
+                nilai[j + 1] = nilai[j];
                 nilai[j] = temp;
             }
         }
     }
     
-    for(int i = 0; i < jumlah; i++){
+    for (int i = 0; i < jumlah; i++) {
         printf("%d\n", nilai[i]);
-        
     }
-        
-        
-        
+    
+    free(nilai);
+    
     return 0;
 }
